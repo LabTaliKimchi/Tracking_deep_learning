43585a3006d39682ca20ffdb0e4ec24a2ee9c705
ArenaZilkha2023 <noga.karp@weizmann.ac.il>
2024-08-06 12:51:02 +0300

    Add GUI

---
+++ MainGui/MainGui/GUI.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 14:04:25 2024
+
+@author: Administrator
+"""
+
+import tkinter as tk
+from tkinter import messagebox
+import subprocess 
+
+
+
+class ColorfulButtonsApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("200x400")  # Width x Height
+        self.root.title("Main GUI")
+
+        # Define button configurations with labels, colors, and callback functions
+        button_configs = [
+            ("1- Annotation", "lightcoral", self.on_red_button_click),
+            ("2- Training", "lightsalmon", self.on_green_button_click),
+            ("3- Prediction", "orchid", self.on_blue_button_click),
+            ("4- Combine prediction with movie", "yellow", self.on_yellow_button_click),
+            ("Purple", "purple", self.on_purple_button_click)
+        ]
+
+        # Create and pack buttons
+        for label, color, command in button_configs:
+            button = tk.Button(self.root, text=label, bg=color, fg="black", padx=50, pady=10, command=command)
+            button.pack(pady=5)
+
+    # Define callback functions for each button
+    def on_red_button_click(self):
+        #run annotation algoritm
+       print(1)
+       self.root.destroy()
+
+    def on_green_button_click(self):
+       #run training algoritm
+      print(2)
+      self.root.destroy()
+
+    def on_blue_button_click(self):
+        #run prediction algoritm
+        print(3)
+        self.root.destroy()
+
+    def on_yellow_button_click(self):
+        #run prediction plus movie algoritm
+        print(4)
+        self.root.destroy()
+
+    def on_purple_button_click(self):
+        messagebox.showinfo("Button Clicked", "Purple button clicked!")
---
+++ MainGui/MainGui/Initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 14:08:05 2024
+
+@author: Administrator
+"""
+import tkinter as tk
+from GUI import ColorfulButtonsApp
+
+
+
+
+
+
+# Create the main window and run the app
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ColorfulButtonsApp(root)
+    root.mainloop()
+    
